322591967efb76a7296dff85591679afb7a6151f
Mona Lisa <soaresmaria181@yahoo.com>
2021-07-25 01:41:48 -0400

    handled errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,9 +3,10 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
-var formSubmitHandler = function (event) {
+var formSubmitHandler = function(event) {
+  // prevent page from refreshing
   event.preventDefault();
-  console.log(event);
+
   // get value from input element
   var username = nameInputEl.value.trim();
 
@@ -13,35 +14,37 @@ var formSubmitHandler = function (event) {
     getUserRepos(username);
 
     // clear old content
-    repoContainerEl.textContent = '';
-    nameInputEl.value = '';
+    repoContainerEl.textContent = "";
+    nameInputEl.value = "";
   } else {
-    alert('Please enter a GitHub username');
+    alert("Please enter a GitHub username");
   }
 };
 
-var getUserRepos = function (user) {
+var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+  // make a get request to url
   fetch(apiUrl)
-    .then(function (response) {
+    .then(function(response) {
       // request was successful
       if (response.ok) {
-        response.json().then(function (data) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
           displayRepos(data, user);
         });
       } else {
-        alert('Error: GitHub User Not Found');
+        alert("Error: " + response.statusText);
       }
     })
-    .catch(function (error) {
-      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    .catch(function(error) {
       alert("Unable to connect to GitHub");
     });
 };
 
-var displayRepos = function (repos, searchTerm) {
+var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
@@ -55,9 +58,10 @@ var displayRepos = function (repos, searchTerm) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    // create a container for each repo
-    var repoEl = document.createElement("div");
+    // create a link for each repo
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
@@ -84,10 +88,7 @@ var displayRepos = function (repos, searchTerm) {
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
   }
-
 };
 
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
-
-
